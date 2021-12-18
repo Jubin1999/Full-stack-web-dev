47554ad7b46051085f632fa5dd32f51faf56b043
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-12-18 09:56:15 +0530

    Add files via upload

---
+++ l12.js
@@ -0,0 +1,11 @@
+function cooking(maggi, pani, tapeli){
+    console.log("Your maggie will be ready in "
+    + maggi * 2
+    +" minutes"
+    + "and Ingredients used are:-  "
+    + maggi + " Maggie"
+    +" "+ pani +" " + "cups of water"
+    + " using " + tapeli + " pan")
+}
+cooking(4, 8, 1);
+
