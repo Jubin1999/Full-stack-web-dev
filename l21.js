628c8440b36effc72629f719c65cafb8f287cd3c
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-12-24 15:09:12 +0530

    Add files via upload

---
+++ l21.js
@@ -0,0 +1,29 @@
+const product = {
+    itemName: 'Flower',
+    price: 50,
+    discount: 20,
+    itemCode: 'F40'
+}
+
+function createProduct(name, price, discount, itemCode){
+    return{
+        itemName: name,
+        price: price,
+        discount: discount,
+        itemCode: itemCode
+    }
+}
+const football = createProduct('football', 400, 10, 'F30');
+
+function Product(name, price, discount, itemCode){
+    this.itemName = name;
+    this.price = price;
+    this.discount = discount;
+    this.itemCode = itemCode;
+    this.discountValue = function(){
+        return price * discount/100;
+    }
+}
+
+const mobile = new Product('OnePlus Nord', 30000, 5, 'OP20')
+
