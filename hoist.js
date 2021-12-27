e5923955eb9677e7b77334121d6748874250e8b0
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-12-27 15:29:08 +0530

    Add files via upload

---
+++ hoist.js
@@ -0,0 +1,18 @@
+// sayHello();
+// console.log(i);
+// var sayHello = function() {
+//     console.log("Hello");
+// }
+
+// var i = 10;
+
+// sayHello();
+// console.log(i);
+
+let foo = 'bar';
+
+if(true){
+    let foo = 'baz';
+    console.log(foo);
+}
+console.log(foo);
